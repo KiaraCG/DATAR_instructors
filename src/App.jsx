c4077c9ce395efcaab332549e41ecab5678ba560
Zeyu Xiong <scyzx2@gmail.com>
2024-09-19 12:44:12 +0200

    update proxy

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
-import React, {useState} from "react";
-import Routes from "./Routes";
-import {BrowserRouter as Router} from "react-router-dom";
-import {ChakraProvider} from "@chakra-ui/react";
-import theme from "./theme";
+import React, { useState } from 'react'
+import Routes from './Routes'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { ChakraProvider } from '@chakra-ui/react'
+import theme from './theme'
+import axios from 'axios'
 
 function App() {
-    // const [user, setUser] = useState('');
+	// const [user, setUser] = useState('');
 
-    return (
-        <ChakraProvider theme={theme}>
-            <Router>
-                <Routes/>
-                {/* <Routes user={user} setUser={setUser} /> */}
-            </Router>
-        </ChakraProvider>
-    );
+	axios.defaults.headers.common['Content-Type'] = 'application/json'
+	axios.defaults.headers.common['Accept'] = 'application/json'
+
+	return (
+		<ChakraProvider theme={theme}>
+			<Router>
+				<Routes />
+				{/* <Routes user={user} setUser={setUser} /> */}
+			</Router>
+		</ChakraProvider>
+	)
 }
 
-export default App;
+export default App
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,67 +1,93 @@
-import { Helmet } from "react-helmet";
-import LoginRowcommentlogin from "./LoginRowcommentlogin";
-import { Button, Text, Flex, Container, Box, Link } from "@chakra-ui/react";
-import React from "react";
-import axios from "axios";
-import {useNavigate} from "react-router-dom";
+import { Helmet } from 'react-helmet'
+import LoginRowcommentlogin from './LoginRowcommentlogin'
+import { Button, Text, Flex, Container, Box, Link } from '@chakra-ui/react'
+import React from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 export default function LoginPage() {
+	const navigate = useNavigate()
 
-    const navigate = useNavigate();
+	const handleLogin = async (username, password) => {
+		try {
+			const response = await axios.get(
+				`http://129.132.15.76:8008/user/info/${username}`
+			)
+			const userData = response.data
 
-    const handleLogin = async (username, password) => {
-        try {
-            const response = await axios.get(`http://129.132.15.76:8008/user/info/${username}`);
-            const userData = response.data;
+			// authenticate password
+			if (userData.password !== password) {
+				throw new Error()
+			}
 
-            // authenticate password
-            if (userData.password !== password) {
-                throw new Error();
-            }
+			localStorage.setItem('user', userData.id) // set user in local storage
+			navigate('/mycustompieces') // go to my custom pieces
+		} catch (error) {
+			console.error('Login failed:', error)
+			alert('Login failed. Please check your username and password.')
+		}
+	}
 
-            localStorage.setItem('user', userData.id); // set user in local storage
-            navigate('/mycustompieces'); // go to my custom pieces
-        } catch (error) {
-            console.error("Login failed:", error);
-            alert("Login failed. Please check your username and password.");
-        }
-    };
-
-
-    return (
-        <>
-            <Helmet>
-                <title>datAR - Login</title>
-            </Helmet>
-            <Box bg="white.a700" w="100%">
-                <Box alignSelf="stretch" >
-                <Flex
-                    borderColor="blue_gray.100_01"
-                    borderBottomWidth="1px"
-                    borderStyle="solid" 
-                    bg="green.a700"
-                    p={{ base: "20px", sm: "24px" }}
-                >
-                    <Container gap="17px"
-                        w="100%"
-                        display="flex" justifyContent="flex-end" alignItems="center" maxW="1152px" px="8px"
-                        mx="auto"
-                        >
-                        <Link href="/" target="_blank" rel="noreferreril" alignself="center">
-
-                        
-                        <Text color="gray.100_01" px="8px" py="4px" borderRadius="8px" _hover={{
-                                color: "black.900", borderTopLeftRadius: 8, borderTopRightRadius: 8, borderBottomLeftRadius: 8, borderBottomRightRadius: 8, bg: "gray.100",
-                            }}>Home</Text>
-                        </Link>
-                        <Link href="/login" target="_blank" rel="noreferreril" alignself="center">
-                        <Button background="white">Login</Button>
-                        </Link>
-                    </Container >
-                </Flex >
-            </Box >
-                <LoginRowcommentlogin onLogin={handleLogin} />
-            </Box>
-        </>
-    );
+	return (
+		<>
+			<Helmet>
+				<title>datAR - Login</title>
+			</Helmet>
+			<Box bg='white.a700' w='100%'>
+				<Box alignSelf='stretch'>
+					<Flex
+						borderColor='blue_gray.100_01'
+						borderBottomWidth='1px'
+						borderStyle='solid'
+						bg='green.a700'
+						p={{ base: '20px', sm: '24px' }}
+					>
+						<Container
+							gap='17px'
+							w='100%'
+							display='flex'
+							justifyContent='flex-end'
+							alignItems='center'
+							maxW='1152px'
+							px='8px'
+							mx='auto'
+						>
+							<Link
+								href='/'
+								target='_blank'
+								rel='noreferreril'
+								alignself='center'
+							>
+								<Text
+									color='gray.100_01'
+									px='8px'
+									py='4px'
+									borderRadius='8px'
+									_hover={{
+										color: 'black.900',
+										borderTopLeftRadius: 8,
+										borderTopRightRadius: 8,
+										borderBottomLeftRadius: 8,
+										borderBottomRightRadius: 8,
+										bg: 'gray.100',
+									}}
+								>
+									Home
+								</Text>
+							</Link>
+							<Link
+								href='/login'
+								target='_blank'
+								rel='noreferreril'
+								alignself='center'
+							>
+								<Button background='white'>Login</Button>
+							</Link>
+						</Container>
+					</Flex>
+				</Box>
+				<LoginRowcommentlogin onLogin={handleLogin} />
+			</Box>
+		</>
+	)
 }
